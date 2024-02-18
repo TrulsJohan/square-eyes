9238effafd42fe94ed3151385b8ff5a55f7e660c
Truls Johan Jacobsen-Lamøy <>
2024-02-18 14:54:37 +0100

    made som small changes

--- home/js/home.js
+++ home/js/home.js
@@ -2,13 +2,20 @@ const productContainer = document.getElementById("#product-container");
 
 let products = [];
 fetch("https://v2.api.noroff.dev/square-eyes")
-    .then((response) => {
-        return response.json();
-    })
-    .then((result) => {
-        products = result;
-    })
-    .then(()=> console.log(products));
+    .then((response) => response.json())
+    .then((result) => products = result)
+    .then(()=> console.log(products))
+    .catch(error => console.log(error));
 
+function displayProducts(product) {
+    const productsContainer = document.createElement("div");
+    const productsTitle = document.createElement("p");
+    productsTitle.innerText = product.title;
+    productsContainer.appendChild(productsTitle);
+}
+
+for (const product of products){
+    displayProducts(product);
+}
 
 
